87faa7d12d6d094765acb45e3f85616bff0d1d94
Minh Dinh <minhdinh1552@gmail.com>
2024-07-20 21:29:28 +0700

    fix buoi7

--- buoi7/src/buoi7/btvn2/BT2Application.java
+++ buoi7/src/buoi7/btvn2/BT2Application.java
@@ -23,40 +23,23 @@ public class BT2Application {
 		System.out.print("\n");
 	}
 	
-	public static int[] timSoLonNhat(int n, int[] arr) {
-		int cnt = 0;
+	public static void timSoLonNhat(int n, int[] arr) {
+		
 		int max = Integer.MIN_VALUE;
-		int index = 0;
+		
 		for (int i=0; i<n; i++) {
 			if (arr[i] >= max) {
 				max = arr[i];
-				index = i;
 			}
 		}
 		
+		System.out.print("Số lớn nhất trong mảng ở vị trí ");
 		for (int i=0; i<n; i++) {
 			if (arr[i] == max) {
-				cnt++;
+				System.out.print(i + " ");
 			}
 		}
-		
-		int[] res;
-		if (cnt > 1) {
-			res = new int[cnt];
-			for (int i=0, j=0; i<n; i++) {
-				if (arr[i] == max) {
-					res[j] = i;
-					j++;
-				}
-			}
-		}
-		else {
-			res = new int[1];
-			res[0] = index;
-		}
-		
-		
-		return res;
+
 	}
 
 	public static int timSoAmDauTien(int n, int[] arr) {
@@ -69,44 +52,28 @@ public class BT2Application {
 		return -1;
 	}
 	
-	public static int[] timSoAmLonNhat(int n, int[] arr) {
-		int cnt = 0;
+	public static void timSoAmLonNhat(int n, int[] arr) {
+		
 		int max = Integer.MIN_VALUE;
-		int index = 0;
+		
 		for (int i=0; i<n; i++) {
 			if (arr[i] < 0 && arr[i] >= max) {
 				max = arr[i];
-				index = 1;
 			}
 		}
 		
+		System.out.print("Số âm lớn nhất trong mảng ở vị trí ");
 		for (int i=0; i<n; i++) {
 			if (arr[i] == max) {
-				cnt++;
+				System.out.print(i + " ");
 			}
 		}
 		
-		int[] res;
-		if (cnt > 1) {
-			res = new int[cnt];
-			for (int i=0, j=0; i<n; i++) {
-				if (arr[i] == max) {
-					res[j] = i;
-					j++;
-				}
-			}
-		}
-		else {
-			res = new int[1];
-			res[0] = index;
-		}
-		
-		
-		return res;
 	}
 	
 	public static int tongSoChan(int n, int[] arr) {
 		int tong = 0;
+		
 		for (int i=0; i<n; i++) {
 			if (arr[i] % 2 == 0)
 				tong += arr[i];
@@ -117,6 +84,7 @@ public class BT2Application {
 	
 	public static int demSoAm(int n, int[] arr) {
 		int dem = 0;
+		
 		for (int i=0; i<n; i++) {
 			if (arr[i] < 0)
 				dem += 1;
@@ -127,6 +95,7 @@ public class BT2Application {
 	
 	public static int tongSoAm(int n, int[] arr) {
 		int tong = 0;
+		
 		for (int i=0; i<n; i++) {
 			if (arr[i] < 0)
 				tong += arr[i];
@@ -152,52 +121,55 @@ public class BT2Application {
 		
 		System.out.println("Nhập số lượng phần tử: ");
 		int n = sc.nextInt();
-		int[] arr = new int[n];
-	
+		if (n <= 0) {
+			System.out.println("Ko hợp lệ");
+		}
+		else {
+			int[] arr = new int[n];
+			
+			
+			nhap(n, arr);
+			xuat(n, arr);
+			
+			// b
+			timSoLonNhat(n, arr);
+
+			
+			// c
+			System.out.println("");
+			int vitri2 = timSoAmDauTien(n, arr);
+			System.out.println("Số âm đầu tiên ở vị trí " + vitri2);
+			
+			
+			// d
+			timSoAmLonNhat(n, arr);
+			
+			
+			// e
+			int tongChan = tongSoChan(n, arr);
+			System.out.println("\nTổng các số chẵn trong mảng là " + tongChan);
+			
+			// f
+			int demAm = demSoAm(n, arr);
+			System.out.println("Có " + demAm + " số âm trong mảng");
+			
+			// g
+			int tongAm = tongSoAm(n, arr);
+			System.out.println("Tổng các số âm trong mảng là " + tongAm);
+			
+			// h 
+			System.out.print("\nNhập x cần tìm: ");
+			int x = sc.nextInt();
+			
+			if (timX(n, arr, x)) {
+				System.out.println(x + " có trong mảng");
+			}
+			else {
+				System.out.println(x + " ko có trong mảng");
+			}
+		}
 		
-		nhap(n, arr);
-		xuat(n, arr);
 		
-		// b
-		int[] vitri = timSoLonNhat(n, arr);
-		System.out.print("Số lớn nhất ở vị trí ");
-		for (int i=0; i<vitri.length; i++)
-			System.out.print(vitri[i] + " ");
-//		
-//		// c
-//		int vitri2 = timSoAmDauTien(n, arr);
-//		System.out.println("Số âm đầu tiên ở vị trí " + vitri2);
-		
-		
-		// d
-		int[] vitri3 = timSoAmLonNhat(n, arr);
-		System.out.print("\nSố âm lớn nhất ở vị trí ");
-		for (int i=0; i<vitri3.length; i++)
-			System.out.print(vitri3[i] + " ");
-		
-		// e
-//		int tongChan = tongSoChan(n, arr);
-//		System.out.println("\nTổng các số chẵn trong mảng là " + tongChan);
-//		
-//		// f
-//		int demAm = demSoAm(n, arr);
-//		System.out.println("Có " + demAm + " số âm trong mảng");
-//		
-//		// g
-//		int tongAm = tongSoAm(n, arr);
-//		System.out.println("Tổng các số âm trong mảng là " + tongAm);
-//		
-//		// h 
-//		System.out.print("\nNhập x cần tìm: ");
-//		int x = sc.nextInt();
-//		
-//		if (timX(n, arr, x)) {
-//			System.out.println(x + " có trong mảng");
-//		}
-//		else {
-//			System.out.println(x + " ko có trong mảng");
-//		}
-//		
 			
 			
 		sc.close();
